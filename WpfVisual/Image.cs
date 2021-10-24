234381099d6b1722ecaf1e1c74151d2bfe76722b
Tom Samwel <s1162729@student.windesheim.nl>
2021-10-24 16:12:30 +0200

    visual

---
+++ WpfVisual/Image.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace WpfVisual
+{
+    class Image
+    {
+        private Dictionary<string, Bitmap> Cache;
+
+        public Bitmap GetSectionImage(string sectionName)
+        {
+            if (!Cache.ContainsKey(sectionName))
+            {
+                Bitmap bitmap = new Bitmap(sectionName);
+                Cache.Add(sectionName, bitmap);
+            }
+
+            return Cache[sectionName];
+        }
+
+        public void EmptyCache() => Cache.Clear();
+    }
+}

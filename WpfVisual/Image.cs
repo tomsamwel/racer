7a9e496ec36bdb0908c8fff4e0a11d81797e720a
Tom Samwel <s1162729@student.windesheim.nl>
2021-10-26 16:52:19 +0200

    image

--- WpfVisual/Image.cs
+++ WpfVisual/Image.cs
@@ -1,23 +1,88 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Color = System.Drawing.Color;
 
 namespace WpfVisual
 {
-    class Image
+    static class Image
     {
-        private Dictionary<string, Bitmap> Cache;
+        private static Dictionary<string, Bitmap> Cache;
 
-        public Bitmap GetSectionImage(string sectionName)
+        public static Bitmap GetEmptyBitmap(int x, int y)
         {
-            if (!Cache.ContainsKey(sectionName))
+            if (Cache.ContainsKey("empty"))
             {
-                Bitmap bitmap = new Bitmap(sectionName);
-                Cache.Add(sectionName, bitmap);
+                return (Bitmap)Cache["empty"].Clone();
             }
+
+            Bitmap bitmap = new Bitmap(x, y);
+
+            // Loop through the images pixels to reset color.
+            for (x = 0; x < bitmap.Width; x++)
+            {
+                for (y = 0; y < bitmap.Height; y++)
+                {
+                    Color newColor = Color.FromArgb(0, 130, 0);
+                    bitmap.SetPixel(x, y, newColor);
+                }
+            }
+
+            Cache.Add("empty", bitmap);
+
+            return (Bitmap)Cache["empty"].Clone();
+
+        }
+
+        public static Bitmap GetSectionImage(string sectionName)
+        {
+            if (Cache.ContainsKey(sectionName))
+            {
+                return Cache[sectionName];
+            }
+
+            Bitmap bitmap = new Bitmap(sectionName);
+            Cache.Add(sectionName, bitmap);
 
             return Cache[sectionName];
         }
 
-        public void EmptyCache() => Cache.Clear();
+        public static void EmptyCache() => Cache.Clear();
+
+        public static BitmapSource CreateBitmapSourceFromGdiBitmap(Bitmap bitmap)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException("bitmap");
+
+            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            BitmapData bitmapData = bitmap.LockBits(
+                rect,
+                ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            try
+            {
+                int size = (rect.Width * rect.Height) * 4;
+
+                return BitmapSource.Create(
+                    bitmap.Width,
+                    bitmap.Height,
+                    bitmap.HorizontalResolution,
+                    bitmap.VerticalResolution,
+                    PixelFormats.Bgra32,
+                    null,
+                    bitmapData.Scan0,
+                    size,
+                    bitmapData.Stride);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+        }
     }
 }
---
+++ WpfVisual/WpfVisual.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Controls.Primitives;
+
+namespace WpfVisual
+{
+    static class WpfVisual
+    {
+        public static void Initialize()
+        {
+
+        }
+
+        public static Bitmap DrawTrack(Track track)
+        {
+
+        }
+    }
+}
